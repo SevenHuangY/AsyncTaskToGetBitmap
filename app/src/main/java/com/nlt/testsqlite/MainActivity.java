995bbbb55f634f62885b1045fc86b0cbabf6367f
seven <huangyawen.happy@163.com>
2016-03-24 14:39:41 +0800

    2. update

--- app/src/main/java/com/nlt/testsqlite/MainActivity.java
+++ app/src/main/java/com/nlt/testsqlite/MainActivity.java
@@ -1,9 +1,11 @@
 package com.nlt.testsqlite;
 
 import android.graphics.Bitmap;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
 import android.widget.ImageView;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -11,9 +13,10 @@ import java.net.URL;
 public class MainActivity extends AppCompatActivity implements singleton.setBitmap
 {
     private static final String TAG = "test";
+    private String url = "http://img.my.csdn.net/uploads/201504/12/1428806103_9476.png";
     private ImageView im;
     private singleton.MyAsyncTask task;
-    private String url = "http://img.my.csdn.net/uploads/201504/12/1428806103_9476.png";
+    private singleton mSingleton;
 
 
     @Override
@@ -24,10 +27,18 @@ public class MainActivity extends AppCompatActivity implements singleton.setBitm
 
         im = (ImageView) findViewById(R.id.im);
 
-        singleton mSingleton = singleton.getInstance();
+        mSingleton = singleton.getInstance();
 
+
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
         try
         {
+            Log.i(TAG, "onResume");
             URL mURL = new URL(url);
             mSingleton.setBitmapInterface(this);
 
@@ -41,15 +52,16 @@ public class MainActivity extends AppCompatActivity implements singleton.setBitm
     }
 
     @Override
-    protected void onDestroy()
+    protected void onPause()
     {
-        super.onDestroy();
-        if(!task.isCancelled())
+        super.onPause();
+        if(task != null && (task.getStatus() == AsyncTask.Status.RUNNING))
         {
             task.cancel(true);
-            Log.i(TAG, "onDestroy");
+            Log.i(TAG, "onPause");
         }
     }
+
 
     @Override
     public void setBitmap(Bitmap bmp)
--- app/src/main/java/com/nlt/testsqlite/singleton.java
+++ app/src/main/java/com/nlt/testsqlite/singleton.java
@@ -74,8 +74,6 @@ public class singleton
             {
                 e.printStackTrace();
             }
-
-
             return bmp;
         }
 
